5a032d14225caf96c5a2cb9d09291a498a5d4189
Eiliv <evd@ssb.no>
2021-07-09 15:04:39 +0200

    another approach, tried to do a more functional apporach

--- tools/downloadQuestionnaireData.js
+++ tools/downloadQuestionnaireData.js
@@ -4,7 +4,6 @@ const protectSecretValue = (val) =>
     val && `${val.slice(0, 5)} .. ${val.slice(-5)}`;
 
 
-
 const sanitizeConfig = (config) =>
     Object.keys(config)
         .reduce((acc, key) => ({
@@ -26,53 +25,70 @@ admin.initializeApp({
     credential: admin.credential.cert(adminConfig)
 });
 
-const fetchQuestionnaireData =  () => {
+const fetchQuestionnaireData = async () => {
 
-    admin.firestore()
+    const usersDocs = await admin.firestore()
         .collection(`/users`)
         .listDocuments()
-        .then(usersDoc => {
-            let questionData = []
-            usersDoc.forEach(userDoc => {
-                questionData.push(userDoc.collection('/questionnaire').listDocuments().then(questsDoc => {
-                    let q = null
-                    questsDoc.forEach(questDoc => {
-                         q =  questDoc.get().then(snap => {
-                            const questData = snap.data();
-                            if(questData && questData.answers) {
-                                //console.log(data.answers)
-                                return userDoc.collection('/profile').listDocuments().then(profileDocs => {
-                                    let f = null;
-                                    profileDocs.forEach((profileDoc, index) => {
-                                        if(profileDoc.id === 'about') {
-                                            f = profileDoc.get().then(snap => {
-                                                const data = snap.get('respondentDetails')
-                                                if (data) {
-                                                    return {
-                                                        respondentId: data.respondentId,
-                                                        ioNumber: data.ioNumber,
-                                                        answers: questData.answers
-                                                    }
-                                                }
-                                            })
-                                        }
-                                    })
-                                    if(f) return f
-                                })
-                            }
-                        })
-                    })
-                    if(q) return q
-                }))
 
-                Promise.all(questionData).then(results => {
-                    fs.writeFile('../questionnaireData.json', JSON.stringify(results.filter(res => res !== undefined)), err => {
-                        if(err) console.log(err)
-                    })
+    const t = await usersDocs.reduce(async (acc, userDoc) => {
+        const respondentId = userDoc.path.split('/')[1]
+        return acc.then(async result => {
+            //console.log(result)
+            const f =  await userDoc.collection('/questionnaire')
+                .listDocuments()
+                .then(async questsRef => {
+                    const quest = await admin.firestore().getAll(...questsRef)
+                    return quest.reduce((acc, q) => {
+                        if(q.exists) {
+                            const qData = q.data()
+                            return (qData && qData.answers)
+                                ? {...acc, respondentId: respondentId, answers:qData.answers}
+                                : acc
+                        }
+                    }, null)
 
                 })
-            });
+            return f ? [...result, f] : result
+        })
+    }, Promise.resolve([]))
+    console.log(t)
+    fs.writeFile('../questionnaireData.json', JSON.stringify(t), err => {
+        if(err) console.log(err)
+    })
+
+
+    /*.then(usersDoc => {
+        usersDoc.forEach(userDoc => {
+            const respondentId = userDoc.path.split('/')[1]
+            userDoc.collection('/questionnaire')
+                .listDocuments()
+                .then(async questRef => {
+                    const allQuests = await admin.firestore().getAll(...questRef)
+                    console.log(allQuests)
+                })
+            /*questionData.push(userDoc.collection('/questionnaire').listDocuments().then(questsDoc => {
+                let q = null
+                questsDoc.forEach(questDoc => {
+                     q =  questDoc.get().then(snap => {
+                        const questData = snap.data();
+                        if(questData && questData.answers) {
+                            //console.log(data.answers)
+                            return {respondentId: questData._path.segments[1], answers: questData.answers}
+                        }
+                    })
+                })
+                if(q) return q
+            }))
+
+            Promise.all(questionData).then(results => {
+                fs.writeFile('../questionnaireData.json', JSON.stringify(results.filter(res => res !== undefined)), err => {
+                    if(err) console.log(err)
+                })
+
+            })
         });
+    });*/
 
 
 };
